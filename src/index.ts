c864b9c22c3a59e90cfe85bb4a05f9679b5ef66b
Konstantin Nikolaev <k.nikolaev@sportxline.com>
2025-03-01 17:20:33 +0300

    feat: default print export

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,50 @@
 import { Table, TTableOptions } from './Elements';
 import type * as Helpers from './helpers';
 
-export namespace printab {
-    export function print<O extends Helpers.TRow>(
-        rows: O[],
-        options?: TTableOptions<O>,
-        target?: Console | HTMLElement | ((data: string) => any),
-    ) {
-        const table = new Table(rows, options);
+export function print<O extends Helpers.TRow>(
+    rows: O[],
+    options?: TTableOptions<O>,
+    target?: Console | HTMLElement | ((data: string) => any),
+) {
+    const table = new Table(rows, options);
 
-        if (!target || target === console) {
-            console.log(table.buildConsole());
-        } else
-        if (typeof target === 'function') {
-            target(table.buildConsole());
-        } else
-        if (target instanceof HTMLElement) {
-            const tmp = document.createElement('span');
-            tmp.innerHTML = table.buildHtml();
-            target.appendChild(tmp.childNodes[0]);
-        } else {
-            throw new Error('printab: Wrong print target');
-        }
-    }
-
-    export function build<O extends Helpers.TRow>(
-        type: 'console' | 'html',
-        rows: O[],
-        options?: TTableOptions<O>,
-    ) {
-        const table = new Table(rows, options);
-
-        if (type === 'console') {
-            return table.buildConsole();
-        }
-
-        return table.buildHtml();
+    if (!target || target === console) {
+        console.log(table.buildConsole());
+    } else
+    if (typeof target === 'function') {
+        target(table.buildConsole());
+    } else
+    if (target instanceof HTMLElement) {
+        const tmp = document.createElement('span');
+        tmp.innerHTML = table.buildHtml();
+        target.appendChild(tmp.childNodes[0]);
+    } else {
+        throw new Error('printab: Wrong print target');
     }
 }
 
-export const print = printab.print;
-export const build = printab.build;
+export function build<O extends Helpers.TRow>(
+    type: 'console' | 'html',
+    rows: O[],
+    options?: TTableOptions<O>,
+) {
+    const table = new Table(rows, options);
 
-export default printab;
+    if (type === 'console') {
+        return table.buildConsole();
+    }
+
+    return table.buildHtml();
+}
+
+type Extra = {
+    print?: typeof print;
+    build?: typeof build;
+}
+
+const dflt: typeof print & Extra = print;
+
+dflt.print = print;
+dflt.build = build;
+
+export default <typeof print & Required<Extra>> dflt;
